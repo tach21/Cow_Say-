c4ec595d3e3db01a49d100fb7cafbf3ae53fca4c
Tacha <degiovanni21tach@gmail.com>
2020-10-06 10:01:39 +0200

    Donuts

---
+++ index.js
@@ -0,0 +1,3 @@
+const path = require('./information.js');
+
+console.log(path.cowsay)
---
+++ information.js
@@ -0,0 +1,9 @@
+const cowsay = require("cowsay");
+
+console.log(cowsay.say({
+	text : "Hello I'm natacha from Nantes Campus!",
+	e : "oO",
+	T : "U "
+}));
+
+module.exports.cowsay;
